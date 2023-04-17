2bb7fe9f9a502e792031f50ff198b3ebf4de97aa
kowada <kowada@mail.ru>
2023-04-17 19:55:16 +0300

    change publications

--- js/script.js
+++ js/script.js
@@ -103,46 +103,6 @@ $(document).ready(function() {
          $(this).toggleClass('active');
          $(this).find(".menu_item_list").show();
        }
-      /* else {
-        if ($(this).hasClass('active')){
-          $(this).removeClass('active');
-          return
-        } else {
-           $(this).toggleClass('active');
-           return
-         }
-         let a = $(this);
-         let b = $(this).children(".menu_item menu_item_end");
-         console.log(a);
-         console.log(b);
-         if ($(this).children("#more").length > 0) {
-          let a = $(this);
-          console.log(a);
-          if ($(this).children("#more").hasClass('active')){
-            console.log(2);
-            $(this).children("#more").removeClass('active');
-            return
-          } else {
-            console.log(3);
-             $(this).children("#more").addClass('active');
-             return
-           }
-         } else {
-          console.log(1);
-         }
-         if ($(this).children("#more").hasClass('active')){
-          $(this).removeClass('active');
-          $(this).find(".menu_item_list").removeClass('active');
-          $(this).find(".menu_item_list").hide();
-          return
-        } if ($(this).children("#more").hasClass('active') === false) {
-           $(this).addClass('active');
-           $(this).find(".menu_item_list").toggleClass('active');
-           $(this).find(".menu_item_list").show();
-           return
-         }
-         return
-      } */
     });
     
 
@@ -172,72 +132,6 @@ $(document).ready(function() {
     }
   });
 
-  /* let counter = 0;
-  let previousWindow;
-
-  checkMenuWidthWhenLoad ($(window).width(), counter);
-
-   $(window).resize(function() {
-    
-    let widthWindow = $(window).width();
-
-    if (previousWindow > widthWindow) {
-     counter = checkMenuWidthWhenResize(widthWindow, counter);
-    }
-    
-    previousWindow = widthWindow;
-  });
-
-  function checkMenuWidthWhenLoad (widthWindow, counter) {
-    if (widthWindow < 1600) {
-      counter = hideMenuItem (1600);
-    }
-
-    if (widthWindow < 1400) {
-      counter = hideMenuItem (1400);
-    }
-
-    if (widthWindow < 1200) {
-      counter = hideMenuItem (1200);
-    }
-  }
-
-  function checkMenuWidthWhenResize (widthWindow, counter) {
-    if (1400 <= widthWindow && widthWindow < 1600 && counter !== 1600) {
-      counter = hideMenuItem (1600);
-    }
-
-    if (1200 <= widthWindow && widthWindow < 1400 && counter !== 1400) {
-      counter = hideMenuItem (1400);
-    }
-
-    if (1000 <= widthWindow && widthWindow < 1200 && counter !== 1200) {
-      counter = hideMenuItem (1200);
-    }
-    return counter;
-  }
-
-  function hideMenuItem (counter) {
-    $('.menu_item_more').show();
-    let menuList = $('.menu_list').children().length;
-    let menuItem = $('.menu_list').children().eq(menuList - 2);
-    let a = menuItem.removeClass().addClass('menu_more');
-    let b = a.find('.menu_item_title').removeClass().addClass('menu_more_title');
-    let c = a.find('.menu_item_list').removeClass().addClass('menu_more_list');
-    $('#more').append(menuItem);
-    return counter;
-  }
-    
-  function shoMenuItem (counter) {
-    let moreList = $('#more').children().length;
-    let moreItem = $('#more').children().eq(moreList - 1);
-    let a = moreItem.removeClass().addClass('menu_item');
-    let b = a.find('.menu_more_title').removeClass().addClass('menu_item_title');
-    let c = a.find('.menu_more_list').removeClass().addClass('menu_item_list');
-    $('.menu_list').append(moreItem);
-    return counter;
-  } */
-
   $('.menu_item_list').parent().addClass('check');
   $('#more').parent().addClass('check');
   $('.menu_more_list').parent().find('.menu_more_title').addClass('check');
@@ -259,16 +153,4 @@ $(document).ready(function() {
     }
   });
  
-  /* $(window).resize(function() {
-    console.log($(window).height());
-    if ($(window).height() < 626) {
-      $('.menu').css('height', '625px');
-      $('.fixed').css('overflow-y', 'scroll');
-    }
-  });
-  $(window).resize();
-  if ($(window).height() < 626) {
-    $('.menu').css('height', '625px');
-    $('.fixed').css('overflow-y', 'scroll');
-  } */
 });
