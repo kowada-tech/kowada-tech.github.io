6eca60caf4ce75958e37c62a8f4b5bfd0a36a53e
kowada <kowada@mail.ru>
2023-04-18 20:57:11 +0300

    change contacts

--- js/script.js
+++ js/script.js
@@ -153,4 +153,9 @@ $(document).ready(function() {
     }
   });
  
+  $('.input-file input[type=file]').on('change', function(){
+    let file = this.files[0];
+    $(this).next().html(file.name);
+  });
+
 });
